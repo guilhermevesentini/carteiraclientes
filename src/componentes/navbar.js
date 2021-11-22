547122f73ea8262f6487cd5e8c496e62b32d4f84
Guilherme Vesentini <guilherme1992@hotmail.com>
2021-11-22 12:12:51 -0300

    first commit

---
+++ src/componentes/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/css/navbar.css';
+import Logo from '../assets/img/novaeralogo.jpg';
+
+function Navbar() {
+    return (
+        <p>guilherminho vicentão da massa porrada nos jogadores de free fire</p>
+    );
+}
+export default Navbar;
